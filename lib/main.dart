12f30e472f7e0b3757b5e1dfe3e4d9ddb7c1cc82
mildman1848 <mildman@mailbox.org>
2025-06-19 19:51:46 +0200

    lib/main.dart erstellen

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter APK Demo',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const HomeScreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Flutter APK Demo')),
+      body: const Center(child: Text('Hello, Flutter 2025!')),
+    );
+  }
+}
